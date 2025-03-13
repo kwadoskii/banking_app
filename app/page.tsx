626f3714bb64b784f8ecf1b9b7e2b573de5fe98a
Austin Ofor <kwadoskii@live.com>
2025-03-13 14:04:51 +0100

    setup 3

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-amber-200/10">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start h-screen">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start h-screen bg-gray-25">
         <div className="h-screen">
-          <p className="text-red-800 text-16 ">hello world</p>
+          <p className="text-red-800 text-16">hello world</p>
           <input type="text" className="input-class" />
         </div>
       </main>
